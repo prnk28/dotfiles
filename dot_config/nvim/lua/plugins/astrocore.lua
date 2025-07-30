45091b49ad2d8d7ba1990bfc99ad6dc94f7aebef
Prad N <prad@sonr.io>
2025-07-30 16:19:46 +0800

    feat: configure tmux session for roadmap project

--- dot_config/nvim/lua/plugins/astrocore.lua
+++ dot_config/nvim/lua/plugins/astrocore.lua
@@ -29,7 +29,7 @@ return {
         dirs = {}, -- list of directory patterns (Ex. { "~/.cargo/*" })
       },
       -- automatically update working directory (update manually with `:AstroRoot`)
-      autochdir = true,
+      autochdir = false,
       -- scope of working directory to change ("global"|"tab"|"win")
       scope = "tab", -- Changed from "win" to "global" to make directory changes apply globally
       -- show notification on every working directory change
