596b9f851626f64b056ffe2647620ec40b8fd5a2
Prad Nukala <prad@sonr.io>
2025-10-08 16:33:02 -0400

    feat(chezmoi): add new configuration files for various services

--- dot_config/qutebrowser/config.py
+++ dot_config/qutebrowser/config.py
@@ -1,7 +1,6 @@
 # pylint: disable=C0111
-c = c  # noqa: F821 pylint: disable=E0602,C0103
-config = config  # noqa: F821 pylint: disable=E0602,C0103
-# pylint settings included to disable linting errors
+
+config.load_autoconfig() # load settings done via the gui
 
 import subprocess
 def read_xresources(prefix):
